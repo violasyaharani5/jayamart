ef22b45e8592fed5c1254c6827aad2c0e2bf1b7e
viola cristiana syaharani <violasyaharani574@gmail.com>
2025-05-01 13:47:26 +0700

    menyesuaikan tampilan diberanda

--- admin/index.php
+++ admin/index.php
@@ -233,7 +233,7 @@
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://instagram.com/meaffq/" target="_blank">Viola Cristiana</a>
+      Designed by <a href="https://instagram.com/vcrstshr_/" target="_blank">Viola Cristiana</a>
     </div>
   </footer><!-- End Footer -->
 
